da0518f2e9c25074925ce97f33e6ca2cd5b0a608
Deren B <47374311+DerenB@users.noreply.github.com>
2024-05-25 21:41:43 -0400

    Day 1 Notes

---
+++ Day-001/Playground001.playground/Contents.swift
@@ -0,0 +1,18 @@
+// Imports collection of code for App building
+import Cocoa
+
+// Changeable variable
+var greeting = "Hello, playground"
+
+// Constant Variable
+let myConst = "This can't change"
+
+// Print
+print(greeting)
+
+// Multi Line String
+let myBigString = """
+Line 1
+Line 2
+Line 3
+"""
