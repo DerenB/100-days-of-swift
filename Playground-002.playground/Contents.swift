5363b74951da1b62731efc0e7fd18d0713451709
Deren B <47374311+DerenB@users.noreply.github.com>
2024-05-27 16:01:20 -0400

    Day 2 Playground

---
+++ Playground-002.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var num = 12
+
+var stringInterpolation = "My Greeting: \" \(greeting) \" was inserted there."
+
+var numberInterpolation = "Add an integer to a string: \(num)"
+
+
+// Checkpoint 1 Assignment
+let temperature = 33.0
+
+var tempInFahrenheit = ((temperature * 9) / 5) + 32
+
+print("Temperature in C: \(temperature)")
+print("Temperature in F: \(tempInFahrenheit)")
