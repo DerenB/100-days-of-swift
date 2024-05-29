9a0ac8dedf5aa0b30bf11f545c8713cf652c9d15
Deren B <47374311+DerenB@users.noreply.github.com>
2024-05-29 15:40:34 -0400

    Emoji Selector

--- Demo-01-Emoji/Demo-01-Emoji/ContentView.swift
+++ Demo-01-Emoji/Demo-01-Emoji/ContentView.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 
+// List of Emojis
+enum Emoji: String, CaseIterable {
+    case 😂, 😎, 😭, 😤
+}
+
 struct ContentView: View {
+    // Reference to emoji list
+    @State var selection: Emoji = .😂
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {
+                        emoji in Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Picker")
+            .padding()
+        }
     }
 }
 
