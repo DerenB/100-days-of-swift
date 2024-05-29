92237fa07a4606d779f5e685df2ec6f21b6975d3
Deren B <47374311+DerenB@users.noreply.github.com>
2024-05-29 15:17:38 -0400

    Emoji Demo App

---
+++ Demo-01-Emoji/Demo-01-Emoji/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Demo-01-Emoji
+//
+//  Created by Deren Bozer on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+// Renders the iPhone Preview
+#Preview {
+    ContentView()
+}
---
+++ Demo-01-Emoji/Demo-01-Emoji/Demo_01_EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_01_EmojiApp.swift
+//  Demo-01-Emoji
+//
+//  Created by Deren Bozer on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_01_EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
