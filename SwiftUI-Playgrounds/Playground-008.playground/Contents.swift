2874a480cfe6c10b5910836370ae5eb2b709d4cc
Deren B <47374311+DerenB@users.noreply.github.com>
2024-06-02 22:13:19 -0400

    Day 8 Continue

--- SwiftUI-Playgrounds/Playground-008.playground/Contents.swift
+++ SwiftUI-Playgrounds/Playground-008.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+// Function Error Throwing
+
+// Create Error Cases:
+enum PasswordError: Error {
+    case short, obvious
+}
+
+
+// Function, add "throw" keyword
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
