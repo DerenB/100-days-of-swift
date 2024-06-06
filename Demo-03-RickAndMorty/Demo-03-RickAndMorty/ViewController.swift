458f25828dffe595205794c2e2062109f1a0c0a1
DerenB <dbozerprogramming@gmail.com>
2024-06-06 11:15:54 -0400

    R&M Demo Project Creation

---
+++ Demo-03-RickAndMorty/Demo-03-RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-03-RickAndMorty
+//
+//  Created by Deren Bozer on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
