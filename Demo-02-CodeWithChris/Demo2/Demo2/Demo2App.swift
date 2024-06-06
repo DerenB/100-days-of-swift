0a7e140fe02a27b6269d96ec5f4a33644ebc7be2
Deren B <47374311+DerenB@users.noreply.github.com>
2024-06-05 21:31:49 -0400

    Demo 2

---
+++ Demo-02-CodeWithChris/Demo2/Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by Deren Bozer on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
