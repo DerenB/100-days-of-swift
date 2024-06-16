042effda4c4b7851f188bbfff574160b286676e7
DerenB <dbozerprogramming@gmail.com>
2024-06-15 20:45:02 -0400

    Adding Animation to the button

--- Demo-05-ToDoList/Demo-05-ToDoList/Views/NoItemsView.swift
+++ Demo-05-ToDoList/Demo-05-ToDoList/Views/NoItemsView.swift
@@ -19,6 +19,7 @@ struct NoItemsView: View {
                     .font(.title)
                     .fontWeight(.semibold)
                 Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
                 NavigationLink(
                     destination: AddViiew(),
                     label: {
@@ -27,10 +28,19 @@ struct NoItemsView: View {
                             .font(.headline)
                             .frame(height: 55)
                             .frame(maxWidth: .infinity)
-                            .background(Color.accentColor)
+                            .background(animate ? Color.red : Color.accentColor)
                             .cornerRadius(10)
                     }
                 )
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0.0,
+                    y: animate ? 50 : 30
+                )
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
             } // End of VStack
             .multilineTextAlignment(.center)
             .padding(40)
@@ -44,7 +54,11 @@ struct NoItemsView: View {
     func addAnimation() {
         guard !animate else { return }
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-            withAnimation(.easeInOut) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
                 animate.toggle()
             }
         }
