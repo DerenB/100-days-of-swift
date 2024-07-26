5e82c9bf06c58f13f4c49416d0f7c60d7a86cf3b
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-25 21:52:35 -0400

    Day 16 Project creation

---
+++ SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Playground-016-WeSplit
+//
+//  Created by Deren Bozer on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello")
+                Text("Hello")
+                Text("Hello")
+            }
+            
+            
+            Section {
+                Text("Hello")
+                Text("Hello")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/Playground_016_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playground_016_WeSplitApp.swift
+//  Playground-016-WeSplit
+//
+//  Created by Deren Bozer on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Playground_016_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
