fc20619137de4c79fe01a67eafde18d299bf0919
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-25 22:08:24 -0400

    Updating Notes

--- SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/ContentView.swift
+++ SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/ContentView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                }
+                
+                Section {
+                    Text("Hello1")
+                    Text("Hello2")
+                    Text("Hello3")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                    Text("Hello")
+                }
             }
-            
-            
-            Section {
-                Text("Hello")
-                Text("Hello")
-            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
