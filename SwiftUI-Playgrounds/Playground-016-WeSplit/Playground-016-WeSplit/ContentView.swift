4cb33a42d80b47ccaf46e5f2b4098f464e7be682
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-27 13:01:56 -0400

    Finishing day 16

--- SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/ContentView.swift
+++ SwiftUI-Playgrounds/Playground-016-WeSplit/Playground-016-WeSplit/ContentView.swift
@@ -9,41 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     
+    /// Example of 1 way binding
     @State var tapCount = 0
+    
+    /// Example of 2 way binding
+    @State private var name = ""
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
     
     var body: some View {
         NavigationStack {
             Form {
+                /// Example of 1 way binding
                 Section {
                     Button("Count: \(tapCount)") {
                         self.tapCount += 1
                     }
                 }
+                
+                /// Example of 2 way binding
                 Section {
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
+                    TextField("Enter your name", text: $name)
+                    name == "" ? Text("Hello User !") : Text("Hello \(name) !")
                 }
                 
                 Section {
-                    Text("Hello1")
-                    Text("Hello2")
-                    Text("Hello3")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
-                    Text("Hello")
+                    Picker("Select your student:", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    ForEach(1..<21) {
+                        Text("Hello \($0)")
+                    }
                 }
             }
             .navigationTitle("SwiftUI")
