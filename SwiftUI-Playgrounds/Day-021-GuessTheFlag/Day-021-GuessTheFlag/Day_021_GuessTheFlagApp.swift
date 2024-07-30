b59f6ae892de961601567a7feb456ae182df91e7
Deren B <47374311+DerenB@users.noreply.github.com>
2024-07-30 19:01:36 -0400

    Project Creation

---
+++ SwiftUI-Playgrounds/Day-021-GuessTheFlag/Day-021-GuessTheFlag/Day_021_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_021_GuessTheFlagApp.swift
+//  Day-021-GuessTheFlag
+//
+//  Created by Deren Bozer on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_021_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
